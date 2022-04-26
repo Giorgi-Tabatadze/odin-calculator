d0ae86506d21c4fafb01d76e758f6b7dd838a5ca
Giorgi-Tabatadze <giorgi.taba@icloud.com>
2022-04-26 16:19:25 +0400

    Create operation functions
    Created main files and wrote 4 functions for basic math operations.

---
+++ javascript.js
@@ -0,0 +1,18 @@
+//Functions that complete basic operations
+
+function add (a, b) {
+  return a + b;
+};
+function substract (a, b){
+  return a - b;
+}
+function multiply (a, b){
+  return a * b;
+}
+function divide (a, b){
+  return a / b;
+}
+
+function operate (operation, a , b){
+  return operation(a, b);
+};
